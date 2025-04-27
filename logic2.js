0e302065ac1519366f5925c5f3b1ed4a739e0165
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2025-04-26 22:47:55 -0600

    added one more letter

---
+++ logic2.js
@@ -0,0 +1,10 @@
+console.log("ui")
+console.log("uu")
+console.log("ua")
+console.log("uk")
+console.log("up")
+console.log("uk")
+
+// git add .
+// git commit -m"MESSAGE HERE"
+// git push origin main
